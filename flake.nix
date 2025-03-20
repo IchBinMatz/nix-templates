598c3fc8dffda760548fc414d92b0d37de8358d6
Matz Momme Klint <ichbin@matzklint.de>
2025-03-20 03:47:28 +0100

    add template to flake

--- flake.nix
+++ flake.nix
@@ -2,12 +2,18 @@
   outputs = {self}: {
     templates = {
       default = {
-        path = ./default;
+        path = ./rust;
         description = ''
           uses flake utils to be system agnostic
         '';
       };
       bevy = {
+        path = ./bevy;
+        description = ''
+          a simple bevy starter template
+        '';
+      };
+      rust = {
         path = ./bevy;
         description = ''
           a simple bevy starter template
